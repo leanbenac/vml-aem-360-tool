6782bef2b3613c4b1333989601ad31f6a3749e5e
Leandro Benac <benacleandro@gmail.com>
2026-06-05 17:29:18 -0300

    update zip filename to reflect project name vml-aem-360-tool

--- pack.ps1
+++ pack.ps1
@@ -1,6 +1,6 @@
 $manifest = Get-Content -Raw -Path manifest.json | ConvertFrom-Json
 $version = $manifest.version
-$zipName = "vml-content-tool-v$version.zip"
+$zipName = "vml-aem-360-tool-v$version.zip"
 
 Write-Host "Creando empaquetado para la versión $version..."
 
